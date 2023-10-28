a46df8164cad4c0d0169c2895116ddaef0147df8
ballinyouup <bryanhuici@gmail.com>
2023-10-28 06:58:49 -0400

    added responsive mobile nav menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,20 @@
+<script lang="ts">
+	let sheetOpen = false;
+	import { Menu } from 'lucide-svelte';
+	import Sheet from './Sheet.svelte';
+</script>
+
 <nav class="w-full h-fit p-4 text-2xl font-bold flex justify-between max-w-7xl self-start">
 	<a href="/">FineTune Helper</a>
 	<!-- <div class="flex w-fit gap-4">
 		<a href="/about">About</a>
 		<a href="/blog">Blog</a>
 	</div> -->
+	<div class="sm:hidden flex">
+		<button on:click={() => (sheetOpen = true)}>
+			<Menu />
+		</button>
+	</div>
 </nav>
+
+<Sheet bind:sheetOpen />
---
+++ src/lib/components/Sheet.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+	export let sheetOpen = false;
+	import Button from '$lib/components/Button.svelte';
+	import { onMount } from 'svelte';
+	import { db } from '$lib/database/database';
+	import type { Document } from '$lib/stores/output';
+	import { X, Edit, Save, Trash, Menu } from 'lucide-svelte';
+	import { page } from '$app/stores';
+	let documents: Document[] = [];
+	let edit: boolean[] = [];
+	onMount(async () => {
+		documents = (await db.table('documents').toArray()) as Document[];
+		edit = Array(documents.length).fill(false);
+	});
+	async function newDocument() {
+		await db.table('documents').add({ id: documents.length, name: 'Untitled', completions: [] });
+		documents.push({ id: documents.length, name: 'Untitled', completions: [] });
+		documents = documents;
+	}
+
+	async function deleteDocument(id: number) {
+		documents = documents.filter((doc) => doc.id !== id);
+		await db.table('documents').delete(id);
+	}
+
+	async function editDocumentName(id: number, name: string) {
+		await db.table('documents').update(id, {
+			name: name
+		});
+		edit = Array(documents.length).fill(false);
+	}
+</script>
+
+<div
+	class="fixed top-0 right-0 w-full h-full bg-background z-40 flex flex-col p-4 items-start overflow-hidden gap-4"
+	style={sheetOpen
+		? 'right: 0;transition: right 0.4s ease-in-out'
+		: 'right: -100vw; transition: right 0.4s ease-in-out'}
+>
+	<div class="flex justify-between w-full z-50">
+		<h6>Documents</h6>
+		<button class="" on:click={() => (sheetOpen = false)}><X /></button>
+	</div>
+	<div class="flex-col flex gap-2 w-full">
+		<Button on:click={async () => await newDocument()}>+ Document</Button>
+		<div class="flex flex-col gap-2">
+			{#each documents as doc, index}
+				<div class="w-full flex items-center justify-between h-10">
+					{#if edit[index] === false}
+						<a
+							class="hover:underline p-2 text-lg h-fit whitespace-nowrap overflow-hidden text-ellipsis"
+							href={`/${doc.id}`}
+						>
+							{doc.name}
+						</a>
+						{#if $page.params.id === doc.id.toString()}
+							<div class="flex items-center gap-2 w-fit">
+								<Button
+									className="px-2 h-fit w-fit"
+									variant="ghost"
+									on:click={() => (edit[index] = true)}
+								>
+									<Edit strokeWidth="2" class="h-5 w-5" />
+								</Button>
+								<Button
+									className="px-2 h-fit w-fit"
+									variant="destructive"
+									on:click={async () => await deleteDocument(doc.id)}
+								>
+									<Trash strokeWidth="2" class="h-5 w-5" />
+								</Button>
+							</div>
+						{:else if $page.params.id === undefined && index === 0}
+							<div class="flex items-center gap-2 w-fit">
+								<Button
+									className="px-2 h-fit w-fit"
+									variant="ghost"
+									on:click={() => (edit[index] = true)}
+								>
+									<Edit strokeWidth="2" class="h-5 w-5" />
+								</Button>
+								<Button
+									className="px-2 h-fit w-fit"
+									variant="destructive"
+									on:click={async () => await deleteDocument(doc.id)}
+								>
+									<Trash strokeWidth="2" class="h-5 w-5" />
+								</Button>
+							</div>
+						{/if}
+					{:else}
+						<input class="p-0.5 w-full text-black text-lg" type="text" bind:value={doc.name} />
+						<div class="flex items-center w-fit gap-2 min-w-max">
+							<Button
+								className="px-2 h-fit w-fit"
+								variant="ghost"
+								on:click={async () => await editDocumentName(doc.id, doc.name)}
+							>
+								<Save strokeWidth="2" class="h-5 w-5" />
+							</Button>
+							<Button
+								className="px-2 h-fit w-fit"
+								variant="ghost"
+								on:click={() => (edit = Array(documents.length).fill(false))}
+							>
+								<X strokeWidth="2" class="h-5 w-5" />
+							</Button>
+						</div>
+					{/if}
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
