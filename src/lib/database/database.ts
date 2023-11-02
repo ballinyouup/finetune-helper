2d4dbebafb22b9738ef26e4462d6c149e12b63e7
ballinyouup <bryanhuici@gmail.com>
2023-11-02 12:06:03 -0400

    db 1.3 -> 1.4

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -2,7 +2,7 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('Documents');
 
-db.version(1.3).stores({
+db.version(1.4).stores({
     documents: '++id, completions, name, format, createdAt',
 });
 
