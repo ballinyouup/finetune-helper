a6ec59b46afcba964918cdf4eb71644529d0eed9
ballinyouup <bryanhuici@gmail.com>
2024-06-06 01:31:04 -0400

    added speed insights

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -6,7 +6,9 @@
 	import { Image } from '@unpic/svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
--- src/routes/documents/+layout.svelte
+++ src/routes/documents/+layout.svelte
@@ -5,7 +5,9 @@
 	import Sidebar from '$lib/components/Sidebar.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
