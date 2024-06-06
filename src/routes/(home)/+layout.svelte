735a91fb6f50c19cac1d0f55ee7c2330e1d2258d
ballinyouup <bryanhuici@gmail.com>
2024-06-06 01:23:46 -0400

    added vercel analytics

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -4,12 +4,16 @@
 	import { Menu } from 'lucide-svelte';
 	import Footer from '$lib/components/Footer/Footer.svelte';
 	import { Image } from '@unpic/svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <!-- Navbar -->
 <nav class="flex h-fit w-full max-w-7xl justify-between self-start p-4 text-2xl font-bold">
 	<div class="flex items-center gap-4">
-		<Image src="/logo.png" alt="FineTune Helper Logo" width="40" height="36" />
+		<Image src="/logo.png" alt="FineTune Helper Logo" width={40} height={36} />
 		<a href="/">FineTune Helper</a>
 	</div>
 	<!-- <div class="flex md:hidden">
