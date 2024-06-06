95ff2bf9f4b1f41a3297a1a69c1d541251eb4dd4
ballinyouup <bryanhuici@gmail.com>
2024-06-06 00:49:00 -0400

    added  req. preprocessor for melt

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import vercel from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: sequence([
+    vitePreprocess(),
+    preprocessMeltUI() // add to the end!
+  ]),
   kit: {
     adapter: vercel(),
   },
